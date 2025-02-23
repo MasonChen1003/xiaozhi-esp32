89e37e813fd5a7aacf994468b7345f200beba10f
Shiun Robot DIY <masonchen1003@gmail.com>
2025-02-23 12:43:13 +0800

    Update lamp.cc
    增加控制紅綠燈模組的寫法

--- main/iot/things/lamp.cc
+++ main/iot/things/lamp.cc
@@ -12,19 +12,39 @@ namespace iot {
 // 这里仅定义 Lamp 的属性和方法，不包含具体的实现
 class Lamp : public Thing {
 private:
-    gpio_num_t gpio_num_ = GPIO_NUM_18;
+    gpio_num_t gpio_num_r = GPIO_NUM_27;
+    gpio_num_t gpio_num_g = GPIO_NUM_16;
+    gpio_num_t gpio_num_y = GPIO_NUM_14;
     bool power_ = false;
 
     void InitializeGpio() {
-        gpio_config_t config = {
-            .pin_bit_mask = (1ULL << gpio_num_),
+        gpio_config_t config_r = {
+            .pin_bit_mask = (1ULL << gpio_num_r),
             .mode = GPIO_MODE_OUTPUT,
             .pull_up_en = GPIO_PULLUP_DISABLE,
             .pull_down_en = GPIO_PULLDOWN_DISABLE,
             .intr_type = GPIO_INTR_DISABLE,
         };
-        ESP_ERROR_CHECK(gpio_config(&config));
-        gpio_set_level(gpio_num_, 0);
+        gpio_config_t config_g = {
+            .pin_bit_mask = (1ULL << gpio_num_g),
+            .mode = GPIO_MODE_OUTPUT,
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE,
+        };
+        gpio_config_t config_y = {
+            .pin_bit_mask = (1ULL << gpio_num_y),
+            .mode = GPIO_MODE_OUTPUT,
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE,
+        };
+        ESP_ERROR_CHECK(gpio_config(&config_r));
+        ESP_ERROR_CHECK(gpio_config(&config_g));
+        ESP_ERROR_CHECK(gpio_config(&config_y));
+        gpio_set_level(gpio_num_r, 0);
+        gpio_set_level(gpio_num_g, 0);
+        gpio_set_level(gpio_num_y, 0);
     }
 
 public:
@@ -37,14 +57,37 @@ public:
         });
 
         // 定义设备可以被远程执行的指令
-        methods_.AddMethod("TurnOn", "打开灯", ParameterList(), [this](const ParameterList& parameters) {
+        methods_.AddMethod("TurnOn_R", "打开紅灯", ParameterList(), [this](const ParameterList& parameters) {
             power_ = true;
-            gpio_set_level(gpio_num_, 1);
+            gpio_set_level(gpio_num_r, 1);
+        });
+        methods_.AddMethod("TurnOn_G", "打开綠灯", ParameterList(), [this](const ParameterList& parameters) {
+            power_ = true;
+            gpio_set_level(gpio_num_g, 1);
+        });
+        methods_.AddMethod("TurnOn_Y", "打开黃灯", ParameterList(), [this](const ParameterList& parameters) {
+            power_ = true;
+            gpio_set_level(gpio_num_y, 1);
         });
 
-        methods_.AddMethod("TurnOff", "关闭灯", ParameterList(), [this](const ParameterList& parameters) {
+        methods_.AddMethod("TurnOff_R", "关闭紅灯", ParameterList(), [this](const ParameterList& parameters) {
             power_ = false;
-            gpio_set_level(gpio_num_, 0);
+            gpio_set_level(gpio_num_r, 0);
+        });
+        methods_.AddMethod("TurnOff_G", "关闭綠灯", ParameterList(), [this](const ParameterList& parameters) {
+            power_ = false;
+            gpio_set_level(gpio_num_g, 0);
+        });
+        methods_.AddMethod("TurnOff_Y", "关闭黃灯", ParameterList(), [this](const ParameterList& parameters) {
+            power_ = false;
+            gpio_set_level(gpio_num_y, 0);
+        });
+
+        methods_.AddMethod("TurnOff_all", "关闭全部的灯", ParameterList(), [this](const ParameterList& parameters) {
+            power_ = false;
+            gpio_set_level(gpio_num_r, 0);
+            gpio_set_level(gpio_num_g, 0);
+            gpio_set_level(gpio_num_y, 0);
         });
     }
 };
